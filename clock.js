6da6058d49c3c2948e8efb3089edd7dd67c9b559
Nathaniel Dominguez <nathaniel.a.dominguez@gmail.com>
2018-07-27 08:33:28 -0700

    changed 1 to 1000 for speed

--- clock.js
+++ clock.js
@@ -10,14 +10,45 @@ var hr = document.getElementById("hour");
 setInterval(function(){
 	sec.style.transform = "rotate(" + secDeg + "deg)";
 	secDeg += 6
-}, 1);
+}, 1000);
 
 setInterval(function(){
 	min.style.transform = "rotate(" + minDeg + "deg)";
 	minDeg += .1
-}, 1);
+}, 1000);
 
 setInterval(function(){
 	hr.style.transform = "rotate(" + hrDeg + "deg)";
 	hrDeg += 0.0083333333
-}, 1); 
+}, 1000); 
+
+// var sec
+
+// function minSecRotation(minSec) {
+	// return (minute/60)*360;
+// }
+
+// function hourRotation(minute) {
+	// return (minute/12)*360;
+// }
+
+// function setPosition(elem, degRotation) {
+	// elem.style.transform = "rotate("+degRotation+"deg)";
+// }
+
+// function update(now){
+	// var curSec = now.getSeconds();
+	// var curMin
+	// var curHr
+	// setPosition(secondhand, minSecRotation(curSec));
+	// setPosition(minutehand, minSecRotation(curMin));
+	// setPosition(hourhand, hourRotation(curHour));
+// }
+
+// function tick() {
+	// var date = new Date();
+	// update(date);
+	// setTimeout(tick, 1000)
+// }
+
+// tick();
